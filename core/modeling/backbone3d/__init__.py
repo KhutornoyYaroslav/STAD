3b2a501e6468f9e4cd88aedfd6016a0b70ab20fa
Yaroslav Khutornoy <morse1995@mail.ru>
2024-11-29 18:33:30 +0300

    Add i3d backbone model

--- core/modeling/backbone3d/__init__.py
+++ core/modeling/backbone3d/__init__.py
@@ -1,6 +1,7 @@
 from torch import nn
 from core.config import CfgNode
 from core.modeling.backbone3d.mobilenetv2 import build_mobilenetv2
+from core.modeling.backbone3d.i3d import build_i3d
 
 
 def build_backbone3d(cfg: CfgNode, *args, **kwargs) -> nn.Module:
@@ -8,6 +9,8 @@ def build_backbone3d(cfg: CfgNode, *args, **kwargs) -> nn.Module:
 
     if arch == 'mobilenetv2':
         backbone3d = build_mobilenetv2(cfg)
+    elif arch == 'i3d':
+        backbone3d = build_i3d(cfg)
     else:
         raise ValueError(f"Backbone3d architecture '{arch}' not found")
     
