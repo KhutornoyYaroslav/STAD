e136193d2fff27ab0779bc4bd9e3b12c04631709
Yaroslav Khutornoy <morse1995@mail.ru>
2024-12-02 17:40:54 +0300

    Fix import

--- eval_ucf.py
+++ eval_ucf.py
@@ -1,9 +1,7 @@
 import tqdm
 import torch
 import argparse
-import numpy # TODO: to np
 import numpy as np
-from torch import nn
 from core.config import CfgNode, cfg
 from core.data import make_data_loader
 from core.utils.checkpoint import CheckPointer
@@ -15,9 +13,9 @@ from core.utils.ops import non_max_suppression, xywh2xyxy
 def smooth(y, f=0.05):
     # Box filter of fraction f
     nf = round(len(y) * f * 2) // 2 + 1  # number of filter elements (must be odd)
-    p = numpy.ones(nf // 2)  # ones padding
-    yp = numpy.concatenate((p * y[0], y, p * y[-1]), 0)  # y padded
-    return numpy.convolve(yp, numpy.ones(nf) / nf, mode='valid')  # y-smoothed
+    p = np.ones(nf // 2)  # ones padding
+    yp = np.concatenate((p * y[0], y, p * y[-1]), 0)  # y padded
+    return np.convolve(yp, np.ones(nf) / nf, mode='valid')  # y-smoothed
 
 
 def compute_ap(tp, conf, pred_cls, target_cls, eps=1e-16):
@@ -33,18 +31,18 @@ def compute_ap(tp, conf, pred_cls, target_cls, eps=1e-16):
         The average precision
     """
     # Sort by object-ness
-    i = numpy.argsort(-conf)
+    i = np.argsort(-conf)
     tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
 
     # Find unique classes
-    unique_classes, nt = numpy.unique(target_cls, return_counts=True)
+    unique_classes, nt = np.unique(target_cls, return_counts=True)
     nc = unique_classes.shape[0]  # number of classes, number of detections
 
     # Create Precision-Recall curve and compute AP for each class
-    p = numpy.zeros((nc, 1000))
-    r = numpy.zeros((nc, 1000))
-    ap = numpy.zeros((nc, tp.shape[1]))
-    px, py = numpy.linspace(0, 1, 1000), []  # for plotting
+    p = np.zeros((nc, 1000))
+    r = np.zeros((nc, 1000))
+    ap = np.zeros((nc, tp.shape[1]))
+    px, py = np.linspace(0, 1, 1000), []  # for plotting
     for ci, c in enumerate(unique_classes):
         i = pred_cls == c
         nl = nt[ci]  # number of labels
@@ -59,24 +57,24 @@ def compute_ap(tp, conf, pred_cls, target_cls, eps=1e-16):
         # Recall
         recall = tpc / (nl + eps)  # recall curve
         # negative x, xp because xp decreases
-        r[ci] = numpy.interp(-px, -conf[i], recall[:, 0], left=0)
+        r[ci] = np.interp(-px, -conf[i], recall[:, 0], left=0)
 
         # Precision
         precision = tpc / (tpc + fpc)  # precision curve
-        p[ci] = numpy.interp(-px, -conf[i], precision[:, 0], left=1)  # p at pr_score
+        p[ci] = np.interp(-px, -conf[i], precision[:, 0], left=1)  # p at pr_score
 
         # AP from recall-precision curve
         for j in range(tp.shape[1]):
-            m_rec = numpy.concatenate(([0.0], recall[:, j], [1.0]))
-            m_pre = numpy.concatenate(([1.0], precision[:, j], [0.0]))
+            m_rec = np.concatenate(([0.0], recall[:, j], [1.0]))
+            m_pre = np.concatenate(([1.0], precision[:, j], [0.0]))
 
             # Compute the precision envelope
-            m_pre = numpy.flip(numpy.maximum.accumulate(numpy.flip(m_pre)))
+            m_pre = np.flip(np.maximum.accumulate(np.flip(m_pre)))
 
             # Integrate area under curve
             #x = numpy.linspace(0, 1, 101)  # 101-point interp (COCO)
             #ap[ci, j] = numpy.trapz(numpy.interp(x, m_rec, m_pre), x)  # integrate
-            ap[ci, j] = numpy.trapz(m_pre, m_rec)  # integrate
+            ap[ci, j] = np.trapz(m_pre, m_rec)  # integrate
 
     # Compute F1 (harmonic mean of precision and recall)
     f1 = 2 * p * r / (p + r + eps)
