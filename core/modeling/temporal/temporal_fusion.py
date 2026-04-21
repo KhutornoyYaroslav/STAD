a4d00ca8808f2b8a98f8025f29a6f8401d8ae5f3
Yaroslav Khutornoy <morse1995@mail.ru>
2026-04-21 19:00:13 +0300

    Add residual type to temporal fusion module

--- core/modeling/temporal/temporal_fusion.py
+++ core/modeling/temporal/temporal_fusion.py
@@ -5,22 +5,33 @@ from typing import Sequence, List
 
 
 class TemporalFusion(nn.Module):
+    valid_residual_types = {
+        'none',
+        'concat'
+    }
+
     def __init__(
             self,
             in_channels: Sequence[int],
             hidden_channels: Sequence[int],
             stateful_training: bool = False,
-            learnable_init_state: bool = False):
+            learnable_init_state: bool = False,
+            residual_type: str = 'none'):
         super(TemporalFusion, self).__init__()
         # check args
         if len(in_channels) != len(hidden_channels):
             raise ValueError("'in_channels' length must be equal 'hidden_channels' length")
+        
+        self.residual_type = residual_type.lower()
+        if self.residual_type not in self.valid_residual_types:
+            raise ValueError(f"Residual type must be one of {self.valid_residual_types}")
 
         # create blocks
         self.in_convs = nn.ModuleList()
         self.rnn_blocks = nn.ModuleList()
         self.out_convs = nn.ModuleList()
         for in_ch, hi_ch in zip(in_channels, hidden_channels):
+            # in conv
             self.in_convs.append(nn.Conv2d(in_ch, in_ch, kernel_size=1))
             block = GRUBlock(
                 in_ch,
@@ -28,8 +39,14 @@ class TemporalFusion(nn.Module):
                 stateful_training,
                 learnable_init_state
                 )
+            # rnn block
             self.rnn_blocks.append(block)
-            self.out_convs.append(nn.Conv2d(2 * in_ch, in_ch, kernel_size=1))
+            # out conv
+            if self.residual_type == 'concat':
+                out = nn.Conv2d(2 * in_ch, in_ch, kernel_size=1)
+            else:
+                out = nn.Identity()
+            self.out_convs.append(out)
 
     def prepare_inference(self, batch_sizes: Sequence[int]):
         assert len(batch_sizes) == len(self.rnn_blocks)
@@ -53,24 +70,27 @@ class TemporalFusion(nn.Module):
             B, T, C, H, W = input.shape
 
             # in conv
-            x = input.view(-1, C, H, W)  # from (b, t, c, h, w) to (b*t, c, h, w)
+            x = input.view(-1, C, H, W)         # from (b, t, c, h, w) to (b*t, c, h, w)
             x = self.in_convs[id](x)
 
             # rnn block
-            x = x.view(B, T, C, H, W)    # from (b*t, c, h, w) to (b, t, c, h, w)
-            x = x.view(B, T, C, -1)      # from (b, t, c, h, w) to (b, t, c, h*w)
-            x = x.permute(0, 3, 1, 2)       # from (b, t, fc, fh*fw) to (b, fh*fw, t, fc)
-            x = x.view(-1, T, C)         # from (b, h*w, t, c) to (b*h*w, t, c)
+            x = x.view(B, T, C, H, W)           # from (b*t, c, h, w) to (b, t, c, h, w)
+            x = x.view(B, T, C, -1)             # from (b, t, c, h, w) to (b, t, c, h*w)
+            x = x.permute(0, 3, 1, 2)           # from (b, t, fc, fh*fw) to (b, fh*fw, t, fc)
+            x = x.view(-1, T, C)                # from (b, h*w, t, c) to (b*h*w, t, c)
             x = self.rnn_blocks[id](x)
 
-            # concat
-            x = x.view(B, -1, T, C)      # from (b*h*w, t, c) to (b, h*w, t, c)
-            x = x.permute(0, 2, 3, 1)       # from (b, h*w, t, c) to (b, t, c, h*w)
-            x = x.view(B, T, C, H, W)    # from (b, t, c, h*w) to (b, t, c, h, w)
-            x = torch.concat([input, x], 2)
-
             # out conv
-            x = x.view(-1, 2*C, H, W)      # from (b, t, 2*c, h, w) to (b*t, 2*c, h, w)
+            x = x.view(B, -1, T, C)             # from (b*h*w, t, c) to (b, h*w, t, c)
+            x = x.permute(0, 2, 3, 1)           # from (b, h*w, t, c) to (b, t, c, h*w)
+            x = x.view(B, T, C, H, W)           # from (b, t, c, h*w) to (b, t, c, h, w)
+
+            if self.residual_type == 'concat':
+                x = torch.concat([input, x], 2)
+                x = x.view(-1, 2*C, H, W)       # from (b, t, 2*c, h, w) to (b*t, 2*c, h, w)
+            else:
+                x = x.view(-1, C, H, W)         # from (b, t, c, h, w) to (b*t, c, h, w)
+
             x = self.out_convs[id](x)
             x = x.view(B, T, C, H, W)    # from (b*t, c, h, w) to (b, t, c, h, w)
 
